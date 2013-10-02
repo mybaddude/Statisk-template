be64ebecdd6f66e42846f24f07fd0a9ac445985c
mybaddude <anderspaaschnielsen@Anders-Paasch-Nielsens-MacBook-Pro.local>
2013-10-02 10:46:48 +0200

    Foundation
    Indledende kode til at lære shizzle

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/content/skin/default/stylesheets/screen.css">
+		<title>Title of the document</title>
+	</head>
+
+	<body>
+		<header>
+			<div class="l-container">
+				<div class="menu"> 
+					<ul>
+						<li><a href="#"><span>home</span></a></li>
+						<li><a href="#"><span>menus</span></a></li>
+						<li><a href="#"><span>the cooks</span></a></li>
+						<li><a href="#"><span>feateured events</span></a></li>
+						<li><a href="#"><span>contact</span></a></li>
+						<li><a href="#"><span>blog</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</header>
+		The content of the document......
+	</body>
+
+</html>
