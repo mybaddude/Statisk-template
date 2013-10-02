ba891e2cf1877fc3312ddd18e61ec3dd44347d4e
mybaddude <anderspaaschnielsen@Anders-Paasch-Nielsens-MacBook-Pro.local>
2013-10-02 14:38:55 +0200

    Structure build and markup
    Lavet strukturen på siden, opbygget mapper og filer i god stylesh!

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 	</head>
 
 	<body>
-		<header>
+		<header class="site-header">
 			<div class="l-container">
-				<div class="menu"> 
+				<div class="l-navigation-horisontal"> 
 					<ul>
 						<li><a href="#"><span>home</span></a></li>
 						<li><a href="#"><span>menus</span></a></li>
@@ -20,7 +20,39 @@
 				</div>
 			</div>
 		</header>
-		The content of the document......
+		<section>
+			<div class="m-featured-image">
+				<img src="/content/media/images/feat-img-top.jpg">
+			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<h1>we are a company of cooks.</h1>
+				<h2>we passionately believe that restaurant quality food can be made in every setting, and we are excited to share it with you.</h2>
+				<p>Whoa Nelly! is a boutique catering company serving Los Angeles and Southern California. We offer local, seasonal, farmers market menus for events large and small. Our long-standing relationships with local farmers ensure that we will bring the best of the market to your table. A sustainable philosophy is at the heart of everything we do, and we are committed to using local produce and sustainably raised meats and fish.</p>
+			</div>
+		</section>
+		<section>
+			<div class="m-featured-image">
+				<img src="/content/media/images/feat-img-top.jpg">
+			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<h1>We believe that the menus for special events should be just that: Special</h1>
+			</div>
+		</section>
+		<section>
+			<div class="m-featured-image">
+				<img src="/content/media/images/feat-img-top.jpg">
+			</div>
+		</section>
+		<section class="m-gallery">
+			<div class="l-container">
+				<h1>photo gallery</h1>
+			</div>
+				<img src="/content/media/images/slider1.jpg">
+		</section>
 	</body>
 
 </html>
