647c4bcb756923c17289c5cf583ef4aab5be3c85
mybaddude <anderspaaschnielsen@Anders-Paasch-Nielsens-MacBook-Pro.local>
2013-10-04 15:21:47 +0200

    Cleanup in the code - Optimization
    Opmized all code, got rid of unesscesary code and split _images into
    smaller modules

--- index.php
+++ index.php
@@ -8,108 +8,199 @@
 	<body>
 		<header class="site-header">
 			<div class="l-container">
-				<div class="l-navigation-horisontal"> 
-					<ul>
-						<li><a href="#"><span>home</span></a></li>
-						<li><a href="#"><span>menus</span></a></li>
-						<li><a href="#"><span>the cooks</span></a></li>
-						<li><a href="#"><span>feateured events</span></a></li>
-						<li><a href="#"><span>contact</span></a></li>
-						<li><a href="#"><span>blog</span></a></li>
-					</ul>
-				</div>
+				<ul class="l-navigation-horizontal">
+					<li><a href="#"><span>home</span></a></li>
+					<li><a href="#"><span>menus</span></a></li>
+					<li><a href="#"><span>the cooks</span></a></li>
+					<li><a href="#"><span>feateured events</span></a></li>
+					<li><a href="#"><span>contact</span></a></li>
+					<li><a href="#"><span>blog</span></a></li>
+				</ul>
 			</div>
 		</header>
 		<section>
-			<div class="m-featured-image">
-				<img src="/content/media/images/feat-img-top.jpg">
-			</div>
-		</section>
-		<section>
-			<div class="l-container">
-				<h1>we are a company of cooks.</h1>
-				<h2>we passionately believe that restaurant quality food can be made in every setting, and we are excited to share it with you.</h2>
-				<p>Whoa Nelly! is a boutique catering company serving Los Angeles and Southern California. We offer local, seasonal, farmers market menus for events large and small. Our long-standing relationships with local farmers ensure that we will bring the best of the market to your table. A sustainable philosophy is at the heart of everything we do, and we are committed to using local produce and sustainably raised meats and fish.</p>
-			</div>
-		</section>
-		<section>
-			<div class="m-featured-image">
-				<img src="/content/media/images/feat-img-top.jpg">
-			</div>
-		</section>
-		<section>
-			<div class="l-container">
-				<h1>We believe that the menus for special events should be just that: Special</h1>
-			</div>
-		</section>
-		<section>
-			<div class="m-featured-image">
-				<img src="/content/media/images/feat-img-top.jpg">
-			</div>
-		</section>
-		<section class="center-h1">
-				<div class="l-container">
-					<h1>photo gallery</h1>
+			<div class="m-background">
+				<div class="featured-image">
+					<a class="logo" href="dev.apaasch.dk:8888"><img src="/content/media/images/logo.svg"></a>
 				</div>
+			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<div class="l-innercontainer">
+					<div class="m-block">
+						<div class="entry-title">
+							<h1>we are a company of cooks.</h1>
+						</div>
+						<h4>we passionately believe that restaurant quality food can be made in every setting, and we are excited to share it with you.</h4>
+						<p>Whoa Nelly! is a boutique catering company serving Los Angeles and Southern California. We offer local, seasonal, farmers market menus for events large and small. Our long-standing relationships with local farmers ensure that we will bring the best of the market to your table. A sustainable philosophy is at the heart of everything we do, and we are committed to using local produce and sustainably raised meats and fish.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="m-featured-image">
+				<img src="/content/media/images/feat-img-top.jpg">
+			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<div class="l-innercontainer">
+					<div class="entry-title">
+						<h1>We believe that the menus for special events should be just that: Special</h1>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="m-featured-image">
+				<img src="/content/media/images/feat-img-top.jpg">
+			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<div class="l-innercontainer">
+					<div class="entry-title text-center">
+						<h1>photo gallery</h1>
+					</div>
+				</div>
+			</div>
 				<img src="/content/media/images/slider1.jpg">
 		</section>
-		<section class="center-h1">
+		<section>
 			<div class="l-container">
-				<h1>featured menus</h1>
-				<img src="content/media/images/feat-menus.jpg">
+				<div class="l-innercontainer">
+					<div class="m-block">
+						<div class="entry-title text-center">
+							<h1>featured menus</h1>
+						</div>
+						<img src="content/media/images/feat-menus.jpg">
+					</div>
+				</div>
 			</div>
 		</section>
-		<section class="center-h1">
-			<div class="m-background">
-				<div class="the-cooks">
-					<div class="l-container">
-						<h1>the cooks</h1>
+		<section class="m-block m-background the-cooks">
+				<div class="l-container">
+					<div class="l-innercontainer">
+						<div class="entry-title text-center">
+							<h1>the cooks</h1>
+						</div>
 						<div class="the-cooks-info">
 						</div>
 					</div>
 				</div>
-			</div>
 		</section>
-		<section class="center-h1">
-			<h1>featured events</h1>
+		<section>
 			<div class="l-container">
-				<div class="m-featured-events">
-					<img src="content/media/images/event1.jpg">
-					<img src="content/media/images/event2.jpg">
-					<img src="content/media/images/event3.jpg">
-					<img src="content/media/images/event4.jpg">
-					<img src="content/media/images/event5.jpg">
+				<div class="l-innercontainer">
+					<div class="m-block m-featured-events">
+						<div class="entry-title text-center">
+							<div class="entry-title text-center">
+								<h1>featured events</h1>
+							</div>
+						</div>
+						<ul>
+							<li>
+								<a href="#">
+									<div class="content">
+										<h2>kimberly & sean</h2>
+									</div>
+									<div class="image">
+										<img src="content/media/images/event1.jpg">
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div class="content">
+										<h2>kimberly & sean</h2>
+									</div>
+									<div class="image">
+										<img src="content/media/images/event2.jpg">
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div class="content">
+										<h2>kimberly & sean</h2>
+									</div>
+									<div class="image">
+										<img src="content/media/images/event3.jpg">
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div class="content">
+										<h2>kimberly & sean</h2>
+									</div>
+									<div class="image">
+										<img src="content/media/images/event4.jpg">
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<div class="content">
+										<h2>kimberly & sean</h2>
+									</div>
+									<div class="image">
+										<img src="content/media/images/event5.jpg">
+									</div>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>	
 		</section>
-		<section class="center-h1">
-			<div class="m-background">
-				<div class="testimonials">
-					<h1>testimonials</h1>
-					<div class="l-container">
-						<div class="testimonials-info">
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section class="center-h1">
-			<h1>press</h1>
+		<section class="m-block m-background testimonials">
 			<div class="l-container">
-				<div class="m-press-box">
-				</div>
-			</div>
-		</section>
-		<section class="center-h1">
-			<div class="m-background">
-				<div class="contact">
-					<h1>contact</h1>
-					<div class="l-container">
-						<div class="contact-info">
-						</div>
+				<div class="l-innercontainer">
+					<div class="entry-title text-center">
+						<h1>testimonials</h1>
+					</div>
+					<div class="testimonials-info">
 					</div>
 				</div>
 			</div>
+		</section>
+		<section>
+			<div class="l-container">
+				<div class="l-innercontainer">
+					<div class="entry-title text-center">
+						<h1>press</h1>
+					</div>
+					<div class="m-press-box">
+						<h1>Heading</h1>
+						<h2>Heading</h2>
+						<h3>Heading</h3>
+						<h4>Heading</h4>
+						<h5>Heading</h5>
+						<h6>Heading</h6>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="m-block m-background contact last">
+					<div class="entry-title text-center">
+						<h1>contact</h1>
+					</div>
+					<div class="l-container">
+						<div class="l-innercontainer">
+							<div class="contact-info">
+							</div>
+						</div>
+						<footer class="l-footer site-footer">
+							<ul class="l-navigation-horizontal">
+								<li><a href="#"><span>facebook</span></a></li>
+								<li><a href="#"><span>twitter</span></a></li>
+								<li><a href="#"><span>instagram</span></a></li>
+								<li><a href="#"><span>pinterest</span></a></li>
+							</ul>
+						</footer>
+					</div>
 		</section>
 	</body>
 
